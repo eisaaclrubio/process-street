73ffd76b8d6a2a8991e6ff658f98355b534de8f9
IsaacLRubio <isaac.lopez@enroutesystems.com>
2018-06-11 14:21:16 -0500

    Form added to component
    Upload file working as expected
    
    Bootstrap added for quick UI modifications

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,31 @@
 function processController($scope, $http) {
-    $scope.myVariable = 'Hello World';
+        // Setting myValues variable  
+        $scope.myValues = {};
+   
+        // Override "add" method to submit after selecting a file
+        $scope.submitFile = function () {
+            $('#fileupload').fileupload({
+                add: function (e, data) {
+                    // Submit and use done to detect when the video was completly uploaded
+                    var jqXHR = data.submit()
+                        .done(function (result, textStatus, jqXHR) {
+                            console.log(result.hashed_id);
+                        });
+                }
+            });
+        };
+    
+        // Add event when Upload button is clicked
+        $('#my-file').change($scope.submitFile); 
+    
+        // Binds the wistia data to fileuploadsubmit so the videos are uploaded correctly
+        $('#fileupload').bind('fileuploadsubmit', function (e, data) {
+            data.formData = {
+                api_password: 'a8f7343a766841df47484280a4eadf94d2e18643dc1d17db5ac96b7d2e47f3df',
+                name: $scope.myValues.videoName,
+                project_id: 'j6f1ab7xks'
+            };
+        });
 };
 
 angular.module('processApp', ['blueimp.fileupload'])
