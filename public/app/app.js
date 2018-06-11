2983e35fdb45a7ca2ebdaa54261a0e4a8e58b43f
IsaacLRubio <isaac.lopez@enroutesystems.com>
2018-06-11 15:36:23 -0500

    jQuery elements replaced by angular.element
    Form removed and fileupload overriden
    
    ProgresBar adjusted to failures
    
    Error handling added when wistia reaches limit

--- public/app/app.js
+++ public/app/app.js
@@ -1,59 +1,69 @@
 function processController($scope, $http) {
-        // Setting myValues variable  
-        $scope.myValues = {};
+    // Setting myValues variable  
+    $scope.myValues = {};
+    $scope.uploadButton = angular.element('#myUpload');
+    $scope.barType = 'progress-bar-success';
+    $scope.progressBar = {'display': 'none'};
 
-        // Starts the embedded video from wistia
-        $scope.playVideo = function(id) {
-            window._wq = window._wq || [];
-            _wq.push({ id: id, 
-                onReady: function(video) {
-                    video.bind('play', function() {
-                        video.time(0);
-                        return video.unbind;
-                    });
-                }
-            });
-        };
-   
-        // Override "add" method to submit after selecting a file
-        $scope.submitFile = function () {
-            $('#fileupload').fileupload({
-                add: function (e, data) {
-                    // If a video exists is removed to start new embedded video
-                    if ($('.wistia_embed').length) {
-                        $('.wistia_embed').remove();
-                    }
-                    $('.panel-body').css('height', 'auto');
-                    // Submit and use done to detect when the video was completly uploaded
-                    var jqXHR = data.submit()
-                        .done(function (result, textStatus, jqXHR) {
-                            $scope.playVideo(result.hashed_id);
-                            $('.panel-body').append('<div class="wistia_embed">&nbsp;</div>');
-                            $('.wistia_embed').addClass('wistia_async_' + result.hashed_id);
-                            $('.panel-body').css('height', '525px');
-                        });
-                }
-            });
-        };
-
-        // Prevent submit when enter key is press
-        $("form").bind("keypress", function (e) {
-            if (e.keyCode == 13) {
-                e.preventDefault();
+    // Starts the embedded video from wistia
+    $scope.playVideo = function(id) {
+        window._wq = window._wq || [];
+        _wq.push({ id: id, 
+            onReady: function(video) {
+                video.bind('play', function() {
+                    video.time(0);
+                    return video.unbind;
+                });
             }
         });
-    
-        // Add event when Upload button is clicked
-        $('#my-file').change($scope.submitFile); 
-    
-        // Binds the wistia data to fileuploadsubmit so the videos are uploaded correctly
-        $('#fileupload').bind('fileuploadsubmit', function (e, data) {
+    };
+
+    // Override fileupload
+    $scope.uploadButton.fileupload({
+        url: 'https://upload.wistia.com/',
+        // Override "add" method to submit after selecting a file
+        add: function (e, data) {
             data.formData = {
                 api_password: 'a8f7343a766841df47484280a4eadf94d2e18643dc1d17db5ac96b7d2e47f3df',
                 name: $scope.myValues.videoName,
                 project_id: 'j6f1ab7xks'
             };
-        });
+            // If a video exists is removed to start new embedded video
+            if (angular.element('.wistia_embed').length) {
+                angular.element('.wistia_embed').remove();
+            }
+            $('.panel-body').css('height', 'auto');
+            // Submit and use done to detect when the video was completly uploaded
+            data.submit().done(function (result, textStatus, jqXHR) {
+                $scope.playVideo(result.hashed_id);
+                $scope.progressBar = {'display': 'none'};
+                $scope.$apply();
+                angular.element('.panel-body').append('<div class="wistia_embed">&nbsp;</div>');
+                angular.element('.wistia_embed').addClass('wistia_async_' + result.hashed_id);
+                angular.element('.panel-body').css('height', '525px');
+            });
+        },
+        // Control to display progress bar
+        start: function (e, data) {
+            $scope.progressBar = {'display': 'block'};
+            $scope.$apply();
+        },
+        // Overwriting progress bar function
+        progress: function (e, data) {          
+            var percentage = data.loaded / data.total * 100;
+            $scope.barType = 'progress-bar-success';
+            $scope.progress = {'width' : percentage + '%'};
+            $scope.$apply();
+        },
+        // Handling limited videos error
+        fail: function (e, data) {          
+            var percentage = 100;
+            $scope.barType = 'progress-bar-danger';
+            $scope.progress = {'width' : percentage + '%'};
+            $scope.$apply();
+            alert('You have reached the limit for uploaded videos');
+        }
+    });
 };
 
 angular.module('processApp', ['blueimp.fileupload'])
